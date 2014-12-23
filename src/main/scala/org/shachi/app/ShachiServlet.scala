b8647177bff1a5f9295d42e81b2165de4651b17d
skozawa <skozawa@hatena.ne.jp>
2014-12-23 21:22:15 +0900

    add index.ssp

--- src/main/scala/org/shachi/app/ShachiServlet.scala
+++ src/main/scala/org/shachi/app/ShachiServlet.scala
@@ -6,12 +6,8 @@ import scalate.ScalateSupport
 class ShachiServlet extends ShachiWebAppStack {
 
   get("/") {
-    <html>
-      <body>
-        <h1>Hello, world!!!</h1>
-        Say <a href="hello-scalate">hello to Scalate</a>.
-      </body>
-    </html>
+    contentType = "text/html"
+    ssp("/index")
   }
   
 }
