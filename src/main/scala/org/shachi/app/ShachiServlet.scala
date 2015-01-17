91ff67e1b764badf0a5ab86d391379b7aa991f86
skozawa <skozawa@hatena.ne.jp>
2015-01-17 14:01:52 +0900

    add DatabaseSessionSupport

--- src/main/scala/org/shachi/app/ShachiServlet.scala
+++ src/main/scala/org/shachi/app/ShachiServlet.scala
@@ -2,8 +2,9 @@ package org.shachi.app
 
 import org.scalatra._
 import scalate.ScalateSupport
+import org.shachi.db.DatabaseSessionSupport
 
-class ShachiServlet extends ShachiWebAppStack {
+class ShachiServlet extends ShachiWebAppStack with DatabaseSessionSupport {
 
   get("/") {
     contentType = "text/html"
